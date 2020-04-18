e58c77493eb4aec07ee99db532c5c684c4a4c338
kubilay yazı <kubilayyazi@hotmail.com>
2020-04-18 22:51:55 +0300

    İş katmanı yazılmaya başlandı

---
+++ Business/Abstract/IBookService.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+   public interface IBookService
+    {
+
+        List<Book> GetAll();
+        Book GetById(int id);
+        List<Book> GetByAuthorId(int id);
+
+        void Add();
+
+        void Delete();
+
+        void Update();
+     }
+}
